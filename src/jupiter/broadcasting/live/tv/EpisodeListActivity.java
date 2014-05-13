91e4aba7b0fb0a6ebe586aad6e5e50338bddb94b
awzurn <awzurn@gmail.com>
2014-05-13 14:34:12 -0500

    Added a new MediaPlayerService, that will play a selected episode in the background of the application to allow users to multitask while listening to shows.
    Also implemented a Resume/Pause button that will stop and start where a
    user last left off.
    
    A notification is also placed in the notification bar when an episode is
    playing.

--- src/jupiter/broadcasting/live/tv/EpisodeListActivity.java
+++ src/jupiter/broadcasting/live/tv/EpisodeListActivity.java
@@ -3,19 +3,32 @@ package jupiter.broadcasting.live.tv;
 
 import java.util.Hashtable;
 import java.util.List;
-import jupiter.broadcasting.live.tv.parser.*;
+
+import jupiter.broadcasting.live.tv.parser.RssHandler;
+import jupiter.broadcasting.live.tv.parser.SaxRssParser;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 /*
  * Copyright (c) 2012 Shane Quigley
  *
@@ -26,12 +39,16 @@ import android.widget.AdapterView.OnItemClickListener;
  */
 public class EpisodeListActivity extends Activity{
 	Hashtable<String, String[]> rssLinkTable;
+	Context mContext;
+	String showName;
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		mContext = this;
 		setContentView(R.layout.episodelist);
 		ListView showsListView = (ListView) findViewById(R.id.episodelist);
-		String feed;
-		feed = this.getIntent().getStringExtra("SHOW_FEED");
+		String feed = this.getIntent().getStringExtra("SHOW_FEED");
+		showName = this.getIntent().getStringExtra("SHOW_NAME");
 		SaxRssParser parser = new SaxRssParser();
 		RssHandler customhandler = new RssHandler("title", "link", 15);
 		parser.setRssHadler(customhandler);
@@ -40,25 +57,44 @@ public class EpisodeListActivity extends Activity{
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 					android.R.layout.simple_list_item_1, android.R.id.text1, episodes);
 		showsListView.setAdapter(adapter);
-		final AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
 		showsListView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
 				final String urls[] = rssLinkTable.get(parent.getAdapter().getItem(position));
 				Toast.makeText(getApplicationContext(),
-						getString(R.string.opening) + parent.getAdapter().getItem(position), Toast.LENGTH_LONG)
+						getString(R.string.opening) + " " + parent.getAdapter().getItem(position), Toast.LENGTH_LONG)
 						.show();
-				alertbox.setTitle(R.string.openepisodetitle);
-				alertbox.setItems(R.array.OpenEpisode, new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-						Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse(urls[which]));
-						if(which == 1){
-							i.setDataAndType(Uri.parse(urls[1]), "audio/mp3");
-						}
-						startActivity(i);
-					}
-				});
-				alertbox.show();
+				
+				//clear the saved preferences, as we're loading a new episode
+				SharedPreferences settings = getSharedPreferences("jupiter.broadcasting.live.tv", Context.MODE_APPEND);
+				settings.edit().clear().clear();
+				stopService(new Intent(mContext, MediaPlayerService.class));
+				
+				Intent intent = new Intent(mContext, MediaPlayerService.class);
+				intent.putExtra("NEW_SHOW", true);
+				intent.putExtra("SHOW_URI", Uri.parse(urls[1]).toString());
+				intent.putExtra("SHOW_INFO", showName);
+				startService(intent);
 			}
 		});
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		  switch (item.getItemId()) {
+		    case R.id.menu_pause:
+		    	stopService(new Intent(this, MediaPlayerService.class));
+		    return true;
+		    case R.id.menu_play:
+		    	startService(new Intent(this, MediaPlayerService.class));
+		    default:
+		      return super.onOptionsItemSelected(item);
+		  }
 	}
 }
